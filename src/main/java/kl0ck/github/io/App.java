041b4d8e80622b68f2fb814ea96ecb2f668ef590
kl0ck <ceklock@gmail.com>
2022-04-23 20:48:05 -0300

    Initial commit

---
+++ src/main/java/kl0ck/github/io/App.java
@@ -0,0 +1,13 @@
+package kl0ck.github.io;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/kl0ck/github/io/AppTest.java
@@ -0,0 +1,20 @@
+package kl0ck.github.io;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
