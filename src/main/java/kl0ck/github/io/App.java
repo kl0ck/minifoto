6d4d89d0f3149f9dedcc642ba1680296b0f7e7b2
kl0ck <ceklock@gmail.com>
2022-04-25 13:29:59 -0300

    listagem de arquivos

--- src/main/java/kl0ck/github/io/App.java
+++ src/main/java/kl0ck/github/io/App.java
@@ -1,13 +1,23 @@
 package kl0ck.github.io;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.stream.Stream;
+import static java.lang.System.out;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        //String path = args[0];
+        //String size = args[1];
+
+        File dir = new File(".");
+        Stream<Path> files = Files.list(dir.toPath());
+        out.println(Arrays.toString(files.toArray()));
+        files.close();
     }
+
 }
